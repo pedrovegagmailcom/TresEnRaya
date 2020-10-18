4356c2808aa56f7a94247fe8f5f2e5f64e1b9638
Pedro Vega casero <pvega@techlabsystems.com>
2020-10-18 12:05:00 +0200

    estadosGanadores

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -13,6 +13,7 @@ namespace TresEnRaya.ViewModels
         public NodoJuego nodoPadre = null;
         public List<NodoJuego> nodos;
         public int[] estado = new int[10] { -1, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        public int[,] estadosGanadores = new int[8,3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 1, 5, 9 }, { 3, 5, 7 }, { 1, 4, 7 }, { 2, 5, 6 }, { 3, 6, 9 } };
         public int nivel;
 
         public NodoJuego(NodoJuego nPadre)
